91c5ed56e3f22441001ef263308dc92c24c5f08d
Vipul Gupta <mail.vipulg@gmail.com>
2023-12-24 23:51:18 +0530

    handle 404 page not found page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function NotFoundPage() {
   return (
-    <h2>NotFoundPage</h2>
+    <div className='page-not-found'>
+      <img src="/404-Page.png" alt="404-page-not-found! Something went wrong!" />
+      <div className='_404-message'>
+      <div>View exciting offers on our <Link to="/">Home Page</Link>.</div>
+      <div>Use the Search bar on top of this page to discover products of interest.</div>
+      </div>
+    </div>
   )
 }
 

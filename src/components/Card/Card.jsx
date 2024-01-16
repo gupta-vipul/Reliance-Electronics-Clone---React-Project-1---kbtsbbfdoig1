ff166a75b2f8696444a1283b4655bab2c9a78fc1
Vipul Gupta <mail.vipulg@gmail.com>
2024-01-16 13:57:09 +0530

    Filter section enabled

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -8,35 +8,77 @@ import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 function ProductCard(props) {
   const navigate = useNavigate();
   const { product } = props;
+  async function addProductToWishlist(id) {
+    try {
+      const response = await fetch(
+        `https://academics.newtonschool.co/api/v1/ecommerce/wishlist/`,
+        {
+          method: "PATCH",
+          headers: {
+            projectID: "kbtsbbfdoig1",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+            // "Access-Control-Allow-Origin": "*", // no use same error persists
+          },
+          body: JSON.stringify({
+            productId: id,
+          }),
+        }
+      );
+      const jsonData = await response.json();
+      // CORS Error -
+      console.log(jsonData);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      // setIsLoading(false);
+    }
+  }
+  function addToWishList(e, id) {
+    e.stopPropagation();
+    if (localStorage.getItem("token")) {
+      addProductToWishlist(id);
+    } else {
+      navigate("/login");
+    }
+  }
+
+  function showProductDetails(id) {
+    window.open(`/product/${id}`, "_blank");
+  }
+
   return (
-    <Link to={`/product/${product._id}`} target="_blank">
-      <div className="card">
-        <div className="img-container">
-          <img
-            src={product.displayImage}
-            alt={product.name}
-            onError={(e) => {
-              e.currentTarget.src = "/img_404.svg";
-            }}
-          />
-        </div>
-        <div className="product-title">{product.name}</div>
-        <div className="product-ratings">
-          <Rating
-            name="read-only"
-            value={product.ratings}
-            style={{ fontSize: "16px" }}
-            readOnly
-          />
-          <span>({product.ratings.toFixed(1)})</span>
-        </div>
-        <div className="product-price">{INRConversion(product.price)}</div>
-        <div className="wishlist-btn">
-          <FavoriteBorderIcon className="wishlist-heart" />
-          <span>Wishlist</span>
-        </div>
+    // <Link to={`/product/${product._id}`} target="_blank">
+    <div className="card" onClick={() => showProductDetails(product._id)}>
+      <div className="img-container">
+        <img
+          src={product.displayImage}
+          alt={product.name}
+          onError={(e) => {
+            e.currentTarget.src = "/img_404.svg";
+          }}
+        />
       </div>
-    </Link>
+      <div className="product-title">{product.name}</div>
+      <div className="product-ratings">
+        <Rating
+          name="read-only"
+          value={product.ratings}
+          style={{ fontSize: "16px" }}
+          readOnly
+        />
+        <span>({product.ratings.toFixed(1)})</span>
+      </div>
+      <div className="product-price">{INRConversion(product.price)}</div>
+      <div
+        className="wishlist-btn"
+        onClick={(e) => addToWishList(e, product._id)}
+      >
+        <FavoriteBorderIcon className="wishlist-heart" />
+        <span>Wishlist</span>
+      </div>
+    </div>
+    // </Link>
   );
 }
 
--- src/pages/ProductContainer.jsx
+++ src/pages/ProductContainer.jsx
@@ -19,6 +19,7 @@ function ProductContainer() {
   const [open, setOpen] = useState(false);
   const [message, setMessage] = useState("");
   const [severity, setSeverity] = useState("info");
+  const [brandKey, setBrandKey] = useState("");
   async function getAllProductsCategoryWise(category) {
     setIsLoading(true);
     try {
@@ -54,7 +55,7 @@ function ProductContainer() {
         },
       });
       const jsonData = await response.json();
-      if(jsonData.status === 'success') {
+      if (jsonData.status === "success") {
         setProducts(jsonData.data);
         const updatedBrand = [
           ...new Set(
@@ -66,8 +67,7 @@ function ProductContainer() {
           ),
         ];
         setBrands(updatedBrand);
-      }
-      else {
+      } else {
         setProducts([]);
       }
     } catch (error) {
@@ -102,30 +102,108 @@ function ProductContainer() {
       setIsLoading(false);
     }
   }
+  async function getFilteredProduct(brand, category) {
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        `https://academics.newtonschool.co/api/v1/ecommerce/electronics/products?filter={"subCategory":"${category}", "brand":"${brand}"}`,
+        {
+          headers: {
+            projectID: "kbtsbbfdoig1",
+          },
+        }
+      );
+      const jsonData = await response.json();
+      setProducts(jsonData.data);
+      setBrandKey(brand);
+      const updatedBrand = [
+        ...new Set(
+          jsonData.data.map((item) => {
+            if (item.brand) {
+              return item.brand;
+            }
+          })
+        ),
+      ];
+      setBrands(updatedBrand);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+  async function getSearchedFilterProducts(brand, userInput) {
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        `https://academics.newtonschool.co/api/v1/ecommerce/electronics/products?search={"name":"${userInput}"}&filter={"brand":"${brand}"}`,
+        {
+          headers: {
+            projectID: "kbtsbbfdoig1",
+          },
+        }
+      );
+      const jsonData = await response.json();
+      if (jsonData.status === "success") {
+        setProducts(jsonData.data);
+        setBrandKey(brand);
+        const updatedBrand = [
+          ...new Set(
+            jsonData.data.map((item) => {
+              if (item.brand) {
+                return item.brand;
+              }
+            })
+          ),
+        ];
+        setBrands(updatedBrand);
+      } else {
+        setProducts([]);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
 
   function handleSnackbarClose(e, reason) {
-    if(reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
     setOpen(false);
   }
   function handlecheckboxClick(e) {
-    console.log(e.target.value)
+    if (productCategory) {
+      getFilteredProduct(e.target.value, productCategory);
+    } else if (userInput) {
+      getSearchedFilterProducts(e.target.value, userInput);
+    } else if (itemsCategories) {
+      // console.log("HomePage data");
+    }
   }
-  // Sorting function 
+  // Sorting function
   function SortingLowToHigh() {
-  //   if(productCategory) {
-      
-  //   }
-  //   else if(userInput) {
-  //     console.log('userInput');
-  //   }
-  //   else if(itemsCategories) {
-  //     console.log('HomePage data');
-  //   }
+    //   if(productCategory) {
+    //   }
+    //   else if(userInput) {
+    //     console.log('userInput');
+    //   }
+    //   else if(itemsCategories) {
+    //     console.log('HomePage data');
+    //   }
   }
   function SortingHighToLow() {
-  //   console.log('price High - low ')
+    //   console.log('price High - low ')
+  }
+
+  function handleFilterClearBtn() {
+    setBrandKey("");
+    if (productCategory) {
+      getAllProductsCategoryWise(productCategory);
+    } else if (userInput) {
+      getSearchResult(userInput);
+    }
   }
 
   useEffect(() => {
@@ -163,8 +241,10 @@ function ProductContainer() {
           <Breadcrumb />
           {products.length == 0 ? (
             <div className="product-not-found-container flex">
-              <div className="product-not-found-image"><img src="/product_not_found.png" alt="product-not-found" /></div>
-              <p>No Result Found, Please try  other search.</p>
+              <div className="product-not-found-image">
+                <img src="/product_not_found.png" alt="product-not-found" />
+              </div>
+              <p>No Result Found, Please try other search.</p>
             </div>
           ) : (
             <div className="product-listing">
@@ -198,23 +278,32 @@ function ProductContainer() {
                   <div className="control-panel">
                     <span>Sort By:</span>
                     <div className="sorting-btn">Relevance</div>
-                    <div className="sorting-btn" onClick={SortingLowToHigh}>Price(Low-High)</div>
-                    <div className="sorting-btn" onClick={SortingHighToLow}>Price(High-Low)</div>
+                    <div className="sorting-btn" onClick={SortingLowToHigh}>
+                      Price(Low-High)
+                    </div>
+                    <div className="sorting-btn" onClick={SortingHighToLow}>
+                      Price(High-Low)
+                    </div>
                   </div>
                 </div>
                 {/* TODO: Work on the applied filters section */}
-                <div className="applied-filter-section">
+                <div
+                  className={`applied-filter-section ${
+                    brandKey ? "" : "afs-display-none"
+                  }`}
+                >
                   <span className="applied-filter-tag text-capitalise">
                     filters:
                   </span>
                   <div className="flex filter-tag">
-                    Sony
+                    {brandKey}
                     <Clear
                       sx={{
                         fontSize: "16px",
                         paddingLeft: "5px",
                         cursor: "pointer",
                       }}
+                      onClick={handleFilterClearBtn}
                     />
                   </div>
                 </div>
@@ -232,12 +321,12 @@ function ProductContainer() {
           )}
         </>
       )}
-      <Toast 
+      <Toast
         open={open}
         duration={5000}
         onClose={handleSnackbarClose}
         severity={severity}
-          message={message}
+        message={message}
       />
     </>
   );
